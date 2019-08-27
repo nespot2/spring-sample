35e2a5e855750fc5e92b69c66d72c8eecb6645b3
leejaehyuk <nespot2@gmail.com>
2019-08-27 20:34:21 +0900

    README.md 파일 추가!

--- src/main/java/com/nespot2/springsample/SecurityConfig.java
+++ src/main/java/com/nespot2/springsample/SecurityConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.expression.SecurityExpressionHandler;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
