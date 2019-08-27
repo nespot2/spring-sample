ed6bb998a5a1341d6b10f410ba8e0945b2927bc8
leejaehyuk <nespot2@gmail.com>
2019-08-28 03:13:48 +0900

    프론트 구성중...

--- src/main/java/com/nespot2/springsample/member/controller/MemberController.java
+++ src/main/java/com/nespot2/springsample/member/controller/MemberController.java
@@ -1,27 +1,13 @@
 package com.nespot2.springsample.member.controller;
 
-import com.nespot2.springsample.member.domain.dto.MemberDto;
-import com.nespot2.springsample.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
-
-@RestController
-@RequestMapping("/member/api")
+@Controller
+@RequestMapping("/member")
 @RequiredArgsConstructor
 public class MemberController {
 
-    private final MemberService memberService;
-
-    @PostMapping
-    public ResponseEntity<String> create(@Valid @RequestBody MemberDto memberDto) {
-        memberService.createMember(memberDto);
-        return ResponseEntity.ok("created");
-    }
 
 }
---
+++ src/test/java/com/nespot2/springsample/Hello.java
@@ -0,0 +1,11 @@
+package com.nespot2.springsample;
+
+import org.junit.Test;
+
+public class Hello {
+
+    @Test
+    public void test(){
+        System.out.println("hello");
+    }
+}
