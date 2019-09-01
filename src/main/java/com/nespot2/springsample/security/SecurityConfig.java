06255b81fe31f00cdfb2f6c49d496aeb074fa5b5
leejaehyuk <nespot2@gmail.com>
2019-09-02 02:04:54 +0900

    회원 가입 기능 추가!

--- src/main/java/com/nespot2/springsample/security/SecurityConfig.java
+++ src/main/java/com/nespot2/springsample/security/SecurityConfig.java
@@ -58,7 +58,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(memberSecurityService);
+        auth.userDetailsService(memberSecurityService).passwordEncoder(passwordEncoder());
     }
 
     @Bean
--- src/main/resources/static/js/ui/member.js
+++ src/main/resources/static/js/ui/member.js
@@ -3,23 +3,17 @@
 
     global.addEventListener('DOMContentLoaded', init);
 
-    /*_.go(
-        $.el(`<div class="signal"></div>`),
-        $.append($.qs(".member"))
-    )*/
-
     function init() {
         const initObjArr = [
             {
                 eventName: "click",
                 el: $.qs("#member_btn"),
-                f: _.debounce((e) => console.log(e), 200)
+                f: _.debounce(createMember, 200)
             },
-
             {
                 eventName: "keydown",
                 el: $.qs("#member_password"),
-                f: _.debounce((e) => console.log(e), 200)
+                f: _.debounce(createMemberWithEnter, 200)
             }
         ];
 
@@ -36,6 +30,57 @@
         )
     }
 
+    function createMemberWithEnter(e) {
+        if (e.keyCode === 13) {
+            createMember(e);
+        }
+    }
+
+    function createMember(e) {
+        _.go(
+            createMemberWithPromise(),
+            ({code}) => {
+                if (code == 0) {
+                    alert("회원 등록!");
+                }
+            }
+        ).catch(({response}) => {
+            loadingRemove($.qs(".signal"));
+            let data = response.data;
+            console.log(data);
+        });
+    }
+
+    function loading(parent) {
+        _.go(
+            $.el(`<div class="signal"></div>`),
+            $.append(parent)
+        )
+    }
+
+    function loadingRemove(el) {
+        _.go(
+            el,
+            $.remove
+        )
+    }
+
+    function createMemberWithPromise() {
+        loading($.qs(".member"));
+        const name = $.qs("#member_name");
+        const password = $.qs("#member_password");
+        const email = $.qs("#member_email");
+
+        const data = {
+            name, password, email
+        };
+
+        return axios({
+            method: "post",
+            data: data,
+            url: "/member-api"
+        });
+    }
 
 }(this, $, _);
 
