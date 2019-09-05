2354acb2f980e61b234426f40eec2f1f6753f5d0
leejaehyuk <nespot2@gmail.com>
2019-09-05 22:53:41 +0900

    application-dev.yml 추가 작업

---
+++ scripts/deploy.sh
@@ -0,0 +1,46 @@
+
+if [ $# -lt 1 ]; then
+	echo "usage: $PROG <host-name>"
+	ehco "	$PROG spring-sample"
+	exit
+fi
+
+deploy() {
+    echo " * Start deploy..."
+
+    while [ $# -gt 0 ]; do
+        local HOST="$1"
+        echo " * Deploy to $1..."
+
+        # Start deploy command
+        ssh -o StrictHostKeyChecking=no ${HOST} mkdir -p bin webapps log
+
+        scp ../scripts/run.sh ${HOST}:bin/${APP_NAME}.sh
+        scp ../build/libs/*.jar ${HOST}:webapps/
+
+        shift
+    done
+
+    echo " * Deploy finished."
+}
+
+function restart() {
+    echo " * Start updating..."
+
+    while [ $# -gt 0 ]; do
+        local HOST="$1"
+        echo " * Restart $1..."
+
+        # Start update command
+        ssh ${HOST} "chmod +x bin/$APP_NAME.sh && bin/$APP_NAME.sh update"
+
+        # End update command
+        shift
+    done
+
+    echo " * All updated."
+}
+
+deploy "$*"
+
+restart "$*"
---
+++ scripts/run.sh
@@ -0,0 +1,88 @@
+SCRIPT_FILE="$(basename $0)"
+PROG="${SCRIPT_FILE%.*}"
+JAR_FILE="$PROG-current.jar"
+
+if [ $# -lt 1 ]; then
+  echo "Usage: $0 [ start | stop | restart ]"
+  exit
+fi
+
+SCRIPT_DIR=$(pwd)
+BASE_DIR=$(dirname $SCRIPT_DIR)
+
+BIN_DIR=$BASE_DIR/bin
+LOG_DIR=$BASE_DIR/log
+APP_DIR=$BASE_DIR/webapps
+
+JAR_FILE=$APP_DIR/$JAR_FILE
+PID_FILE="${LOG_DIR}/.${PROG}.pid"
+
+PROFILE=$(cat "${APP_DIR}/${PROG}.profile")
+
+status() {
+  local PID=$(cat "$PID_FILE")
+  if kill -0 "$PID" >/dev/null 2>&1; then
+    echo " * The process($PID) is alive."
+  else
+    echo " * The process is not alive."
+    rm -f "$PID_FILE"
+  fi
+}
+
+stop() {
+  if [ -f "$PID_FILE" ]; then
+    local SHEEP=0
+    local PID=$(cat "$PID_FILE")
+    if kill -0 "$PID" >/dev/null 2>&1; then
+      echo " * Trying to shutdown..."
+      kill "$PID"
+      while sleep 1; do
+        kill -0 "$PID" >/dev/null 2>&1 || break
+        echo "$((++SHEEP)) sec..."
+      done
+    else
+      echo " * The process is not alive."
+    fi
+
+    rm -f "$PID_FILE"
+  else
+    echo " * The process-id not found."
+  fi
+}
+
+start() {
+  echo " * Starting $PROG [$(basename "$JAR_REAL_FILE")]"
+
+  local LOG_FILE="$LOG_DIR/$PROG.log"
+  local ERR_FILE="$LOG_DIR/$PROG-err.log"
+
+  local OPTS="-Dspring.profiles.active=$PROFILE"
+
+  nohup java $OPTS -jar "$JAR_REAL_FILE" >>$LOG_FILE 2>>$ERR_FILE </dev/null &
+  local PID=$!
+
+  sleep 5
+
+  if kill -0 "$PID"; then
+    echo "$PID" >"$PID_FILE"
+    echo " * Started successfully. $PID"
+  else
+    echo " * Start failed."
+  fi
+}
+
+restart() {
+  stop && start
+}
+
+update() {
+  echo " * Updating..."
+
+  ln -sf $LATEST_FILE $JAR_FILE
+  JAR_REAL_FILE=$(readlink -f -n "$JAR_FILE")
+
+  restart
+}
+
+LATEST_FILE="$(ls -1 $APP_DIR/$PROG-[0-9]*.jar | sort -r -u | head -n 1)"
+
