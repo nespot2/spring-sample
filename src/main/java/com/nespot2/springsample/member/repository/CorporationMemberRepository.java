4f3e7dea7b9d02c0807f87c85e61ed06b093e626
leejaehyuk <nespot2@gmail.com>
2019-09-08 23:04:24 +0900

    법인회원 삭제!

--- src/main/java/com/nespot2/springsample/member/repository/CorporationMemberRepository.java
+++
@@ -1,8 +0,0 @@
-package com.nespot2.springsample.member.repository;
-
-import com.nespot2.springsample.member.domain.CorporationMember;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CorporationMemberRepository extends JpaRepository<CorporationMember, Long> {
-
-}
